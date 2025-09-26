5dc12b86e14a4bf29d90282699e7f779b801353e
tchimi <tchimivialy20@gmail.com>
2025-09-26 08:21:44 -0400

    fixe cors

--- src/main/java/maoi/platforme/securite/CorsConfig.java
+++ src/main/java/maoi/platforme/securite/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
